bcb998a40a465b280a6f06fa0456366bf6ef50c6
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2021-11-21 13:58:48 -0300

    configure dotenv on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack')
+const dotenv = require('dotenv')
 
 module.exports = {
     entry: "./src/index.tsx",
@@ -34,5 +36,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "src", "index.html"),
         }),
+         new webpack.DefinePlugin({
+       'process.env': JSON.stringify(dotenv.config().parsed)}),
     ],
 };
